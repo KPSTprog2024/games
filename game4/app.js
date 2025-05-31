9c6ab24dcf0a900b50ae413f86750640c8144c79
KPSTprog2024 <keep.starting@gmail.com>
2025-05-31 23:51:02 +0900

    Update app.js

--- game4/app.js
+++ game4/app.js
@@ -10,12 +10,12 @@
 const gameConfig = {
   canvasWidth: 600,
   canvasHeight: 400,
-  lineWidth: 12,
+  lineWidth: 8,
   shapeColor: '#888888',
   userTraceColor: '#0066ff',
   vertexPassedColor: '#22cc88',
-  vertexAllowableDistance: 12,
-  edgeAllowableDistance: 24,
+  vertexAllowableDistance: 7,
+  edgeAllowableDistance: 10,
 };
 
 /* ---------- 形 ---------- */
