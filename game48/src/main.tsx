e5a02d19143164872c8f7a4e4f032873d4a48fdd
KPSTprog2024 <keep.starting@gmail.com>
2025-11-24 17:53:45 +0900

    Set up Vite build for game48

---
+++ game48/src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './main.css';
+import GameScreen from './GameScreen';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <GameScreen />
+  </React.StrictMode>,
+);
